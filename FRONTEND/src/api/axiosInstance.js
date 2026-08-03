4e81f4345ac00183fa8ffd183a0b4712fb2beada
SumitSawaria <sumitsawaria132004@gmail.com>
2026-08-03 16:13:25 +0530

    Update base URL for axios instance

--- FRONTEND/src/api/axiosInstance.js
+++ FRONTEND/src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const baseURL = import.meta.env.VITE_API_BASE_URL || "https://quick-link-g8rr.onrender.com";
 
 const axiosInstance = axios.create({
     baseURL,
